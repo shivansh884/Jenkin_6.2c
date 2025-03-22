b23e840ccd1d0d30893b39c1d0f724280b632f39
Shivansh singh <shivansh4814.be23@chitkara.edu.in>
2025-03-22 10:56:23 +0530

    Update home.jsx

--- home.jsx
+++ home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MyComponent = () => {
   return (
     <div>
-      <h1>Welcome to My React App</h1>
+      <h1>Welcome to My JENKIN App</h1>
       <p>This is a simple JSX example.</p>
     </div>
   );
