68959cabcad15bebf87488ce93cc0b8de8c88af7
Shivansh singh <shivansh4814.be23@chitkara.edu.in>
2025-03-22 10:34:28 +0530

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple JSX example.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
